7ed01d2b4088ef8504af69578cc9be6cb094375b
mustafa <mustafa.sadriwala@utdallas.edu>
2020-05-23 03:26:26 -0500

    added formatted content to project cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,7 +7,7 @@ import { StyledLink } from "baseui/link";
 import {Layer as div} from 'baseui/layer';
 import {StatefulMenu} from 'baseui/menu';
 import {Card, StyledContents, StyledBody} from 'baseui/card';
-import { Paragraph1, H1, H2, H5, H6, H4 } from 'baseui/typography';
+import { Paragraph1, Paragraph2, H1, H2, H5, H6, H4, ParagraphSmall } from 'baseui/typography';
 import { Tag, VARIANT} from 'baseui/tag';
 import {FlexGrid, FlexGridItem} from 'baseui/flex-grid';
 
@@ -18,12 +18,13 @@ import SectionTitle from './sectionTitle';
 import CARD_PROJECTS from './../assets/projects-metadata.json';
 
 
-
 export default function Projects(props) {
     const [css, theme] = useStyletron();
+    const [cardProjects, setCardProjects] = useState(CARD_PROJECTS);
     const accentColor = (props.lightTheme ? theme.colors.accent50 : theme.colors.accent600)
     const hoverAccent = (props.lightTheme ? theme.colors.accent100 : theme.colors.accent500)
 
+    const Star = <svg height="100%" viewBox="0 -10 511.98685 511" width="100%" xmlns="http://www.w3.org/2000/svg"><path d="m510.652344 185.902344c-3.351563-10.367188-12.546875-17.730469-23.425782-18.710938l-147.773437-13.417968-58.433594-136.769532c-4.308593-10.023437-14.121093-16.511718-25.023437-16.511718s-20.714844 6.488281-25.023438 16.535156l-58.433594 136.746094-147.796874 13.417968c-10.859376 1.003906-20.03125 8.34375-23.402344 18.710938-3.371094 10.367187-.257813 21.738281 7.957031 28.90625l111.699219 97.960937-32.9375 145.089844c-2.410156 10.667969 1.730468 21.695313 10.582031 28.09375 4.757813 3.4375 10.324219 5.1875 15.9375 5.1875 4.839844 0 9.640625-1.304687 13.949219-3.882813l127.46875-76.183593 127.421875 76.183593c9.324219 5.609376 21.078125 5.097657 29.910156-1.304687 8.855469-6.417969 12.992187-17.449219 10.582031-28.09375l-32.9375-145.089844 111.699219-97.941406c8.214844-7.1875 11.351563-18.539063 7.980469-28.925781zm0 0" fill={theme.colors.warning300}/></svg>
     const StyledDiv = styled('div', () =>{ 
         return {
         height: '100%',
@@ -32,6 +33,7 @@ export default function Projects(props) {
         borderRadius: '10px',
         position: "relative",
         transition: 'ease .2s',
+        paddingTop: '15px',
         ':hover' : {
             transition: 'ease .2s',
             '::before' : {
@@ -41,7 +43,10 @@ export default function Projects(props) {
                 height: '104%',
                 border: '10px solid ' + hoverAccent,
                 borderRadius: '25px',
+                boxShadow: 'rgba(0, 0, 0, 0.16) 0px 4px 16px',
             },
+            //boxShadow: 'rgba(0, 0, 0, 0.16) 0px 4px 16px',
+            borderRadius: '25px',
             backgroundColor: hoverAccent
         },
         '::before' : {
@@ -68,21 +73,68 @@ export default function Projects(props) {
             <FlexGrid
             flexGridColumnCount={[1, 1, 2, 3]}
             flexGridColumnGap="scale800"
-            flexGridRowGap="scale800"
+            flexGridRowGap="scale1000"
             display="flex"
             marginLeft="5vw"
             marginRight="5vw"
             >
-            {CARD_PROJECTS.map((project, index) => (
-                <FlexGridItem   height='scale4800'
-                >
+            {cardProjects.map((project, index) => (
+                <FlexGridItem   height='40vh' display="flex">
                 <StyledDiv>
-                    <Grid gridGutters={[8,16,32]} gridMargins={[8,16,32]} gridGaps={18}>
-                        <Cell>
-                            <H5 style={{marginTop: 5}}>
-                            {project.name}
+                    <Grid gridMargins={[6,12,24]} gridGaps={0} gridGutters={0}>
+                        <Cell span={[3,7,9]}>
+                            <H5 style={{marginTop: 0, marginBottom: 0}}>
+                                {project.name}
                             </H5>
                         </Cell>
+                        <Cell span={[1,1,3]} align={ALIGNMENT.start}>
+                            <ParagraphSmall style={{marginTop: 0, marginBottom: 0}}>
+                            <div style={{marginTop: 0, fontSize: '.8em', display: 'flex', justifyContent: 'flex-end'}}>
+                                <em>{project.dateString}</em>
+                            </div>
+                            </ParagraphSmall>
+                        </Cell>
+                    </Grid>
+                    <Grid gridMargins={[6,12,24]} gridGaps={0} gridGutters={0}>
+                        <Cell span={[4,8,12]}>
+                            <Paragraph1 style={{marginTop: 0, marginBottom: 0}}>
+                                <em>{project.organization ? (project.organization) : (<div>&nbsp;</div>)}</em>
+                            </Paragraph1>
+                        </Cell>
+                    </Grid>
+                    <Grid gridMargins={[6,12,24]} gridGaps={0} gridGutters={0}>
+                        <Cell span={[4,8,12]}>
+                            <ParagraphSmall style={{marginTop: 0, marginBottom: 0}}>
+                                {project.description.substring(0, 200) + "..."}
+                            </ParagraphSmall>
+                        </Cell>
+                    </Grid>
+                    <Grid gridMargins={[3,6,12]} gridGaps={0} gridGutters={0} align={ALIGNMENT.end}
+                    overrides={{
+                        Grid: {
+                            style: () => ({
+                                position: "absolute",
+                                bottom: "5px"
+                            })
+                        }
+                    }}>
+                    <div style={{height: '100%', display: 'flex', alignItems: 'flex-end'}}>
+                        <Cell span={[4,8,12]}>
+                            {project.tags.map((tag, index) => (
+                                <Tag closeable={false}
+                                overrides={{
+                                    Text: {
+                                      style: ({ $theme }) => {
+                                        return {
+                                          fontSize: ".8em",
+                                        };
+                                      }
+                                    }
+                                  }}
+                                >{tag}</Tag>
+                            ))}
+                        </Cell>
+                        </div>
                     </Grid>
                 </StyledDiv>
                 </FlexGridItem>
