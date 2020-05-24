bac36bb6e2153145eb87570b93cd96b97a5c0f9f
mustafa <mustafa.sadriwala@utdallas.edu>
2020-05-23 19:41:19 -0500

    added basic formatted modals for projects on click

---
+++ src/components/modal.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {useStyletron, styled} from 'baseui';
+import {Grid, Cell, BEHAVIOR, ALIGNMENT} from 'baseui/layout-grid';
+import {Heading, HeadingLevel} from 'baseui/heading';
+import Typist from 'react-typist';
+import { StyledLink } from "baseui/link";
+import {Layer as div} from 'baseui/layer';
+import {StatefulMenu} from 'baseui/menu';
+import {Card, StyledContents, StyledBody} from 'baseui/card';
+import { Paragraph1, Paragraph2, H1, H2, H5, H6, H4, ParagraphSmall } from 'baseui/typography';
+import { Tag, VARIANT} from 'baseui/tag';
+import {FlexGrid, FlexGridItem} from 'baseui/flex-grid';
+
+
+
+
+export default function Modal({isShowing, hide, project, lightTheme}) {
+    const [css, theme] = useStyletron();
+    const accentColor = (lightTheme ? theme.colors.accent50 : theme.colors.accent600)
+    const filterColor = (lightTheme ? theme.colors.accent700 : theme.colors.accent600)
+
+    const SelectedDiv = styled('div', () => {
+        return {
+            height: '60vh',
+            width: '80vw',
+            backgroundColor: accentColor,
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            bottom: '50%',
+            right: '50%',
+            transform: 'translate(-50%, -50%)',
+            borderRadius: '20px',
+            zIndex: 1041,
+            padding: '20px'
+        }
+    });
+
+    const WrapperDiv = styled('div', () => {
+        return {
+            width:"100vw",
+            height:"100vh",
+            zIndex: 1039,
+            position:"fixed",
+            top: 0,
+            left: 0
+        }
+    })
+
+    const BackgroundDiv = styled('div', () => {
+        return {
+            width: '100%',
+            height: '100%',
+            zIndex: 1040,
+            position: 'fixed',
+            backgroundColor: filterColor,
+            opacity: .5,
+            top: 0,
+            left: 0,
+        }
+    })
+    
+    return(
+        isShowing ? ReactDOM.createPortal(
+            <React.Fragment>
+                <WrapperDiv>
+                <BackgroundDiv onClick={hide}/>
+                <SelectedDiv>
+                        <Grid gridMargins={[6,12,24]} gridGaps={0} gridGutters={0}>
+                        <Cell span={[3,7,9]}>
+                            <H5 style={{marginTop: 0, marginBottom: 0}}>
+                                {project.name}
+                            </H5>
+                        </Cell>
+                        <Cell span={[1,1,3]} align={ALIGNMENT.start}>
+                            <Paragraph1 style={{marginTop: 0, marginBottom: 0}}>
+                            <div style={{marginTop: 0, fontSize: '.8em', display: 'flex', justifyContent: 'flex-end'}}>
+                                <em>{project.dateString}</em>
+                            </div>
+                            </Paragraph1>
+                        </Cell>
+                    </Grid>
+                    <Grid gridMargins={[6,12,24]} gridGaps={0} gridGutters={0}>
+                        <Cell span={[4,8,12]}>
+                            <Paragraph1 style={{marginTop: 0, marginBottom: 0}}>
+                                <em>{project.organization ? (project.organization) : (<div>&nbsp;</div>)}</em>
+                            </Paragraph1>
+                        </Cell>
+                    </Grid>
+                    <Grid gridMargins={[6,12,24]} gridGaps={0} gridGutters={0}>
+                        <Cell span={[2,5,8]}>
+                            <Paragraph1 style={{marginBottom: 0}}>
+                                {project.description}
+                            </Paragraph1>
+                        </Cell>
+                        <Cell span={[2,3,4]} align={ALIGNMENT.center}>
+                            <div style={{height: '50%', fontSize: '.8em', display: 'flex', position: 'absolute'}}>
+                            <img style={{objectFit: 'contain', width: '100%'}} src={project.img}></img>
+                            </div>
+                        </Cell>
+                    </Grid>
+                    <Grid>
+
+                    </Grid>
+                    <button type="button" onClick={hide}>
+                            close
+                    </button>
+                    <Grid gridMargins={[3,6,12]} gridGaps={0} gridGutters={0} align={ALIGNMENT.end}
+                    overrides={{
+                        Grid: {
+                            style: () => ({
+                                position: "absolute",
+                                bottom: "10px"
+                            })
+                        }
+                    }}>
+                        <Cell span={[4,8,12]}>
+                            {project.tags.map((tag, index) => (
+                                <Tag closeable={false}
+                                index={index}
+                                overrides={{
+                                    Text: {
+                                      style: () => {
+                                        return {
+                                          fontSize: ".8em",
+                                        };
+                                      }
+                                    }
+                                  }}
+                                >{tag}</Tag>
+                            ))}
+                        </Cell>
+                    </Grid>
+                </SelectedDiv>
+                </WrapperDiv>
+            </React.Fragment>, document.body
+        ): null
+    );
+}
--- src/components/projects.js
+++ src/components/projects.js
@@ -12,6 +12,8 @@ import { Tag, VARIANT} from 'baseui/tag';
 import {FlexGrid, FlexGridItem} from 'baseui/flex-grid';
 
 
+import useModal from './useModal';
+import Modal from './modal';
 import SectionTitle from './sectionTitle';
 
 
@@ -21,10 +23,14 @@ import CARD_PROJECTS from './../assets/projects-metadata.json';
 export default function Projects(props) {
     const [css, theme] = useStyletron();
     const [cardProjects, setCardProjects] = useState(CARD_PROJECTS);
+    const [selectedProjectIndex, setSelectedProjectIndex] = useState(-1);
+    const [selectedProject, setSelectedProject] = useState('');
+    const {isShowing, toggle} = useModal();
+
     const accentColor = (props.lightTheme ? theme.colors.accent50 : theme.colors.accent600)
     const hoverAccent = (props.lightTheme ? theme.colors.accent100 : theme.colors.accent500)
 
-    const Star = <svg height="100%" viewBox="0 -10 511.98685 511" width="100%" xmlns="http://www.w3.org/2000/svg"><path d="m510.652344 185.902344c-3.351563-10.367188-12.546875-17.730469-23.425782-18.710938l-147.773437-13.417968-58.433594-136.769532c-4.308593-10.023437-14.121093-16.511718-25.023437-16.511718s-20.714844 6.488281-25.023438 16.535156l-58.433594 136.746094-147.796874 13.417968c-10.859376 1.003906-20.03125 8.34375-23.402344 18.710938-3.371094 10.367187-.257813 21.738281 7.957031 28.90625l111.699219 97.960937-32.9375 145.089844c-2.410156 10.667969 1.730468 21.695313 10.582031 28.09375 4.757813 3.4375 10.324219 5.1875 15.9375 5.1875 4.839844 0 9.640625-1.304687 13.949219-3.882813l127.46875-76.183593 127.421875 76.183593c9.324219 5.609376 21.078125 5.097657 29.910156-1.304687 8.855469-6.417969 12.992187-17.449219 10.582031-28.09375l-32.9375-145.089844 111.699219-97.941406c8.214844-7.1875 11.351563-18.539063 7.980469-28.925781zm0 0" fill={theme.colors.warning300}/></svg>
+    //const Star = <svg height="100%" viewBox="0 -10 511.98685 511" width="100%" xmlns="http://www.w3.org/2000/svg"><path d="m510.652344 185.902344c-3.351563-10.367188-12.546875-17.730469-23.425782-18.710938l-147.773437-13.417968-58.433594-136.769532c-4.308593-10.023437-14.121093-16.511718-25.023437-16.511718s-20.714844 6.488281-25.023438 16.535156l-58.433594 136.746094-147.796874 13.417968c-10.859376 1.003906-20.03125 8.34375-23.402344 18.710938-3.371094 10.367187-.257813 21.738281 7.957031 28.90625l111.699219 97.960937-32.9375 145.089844c-2.410156 10.667969 1.730468 21.695313 10.582031 28.09375 4.757813 3.4375 10.324219 5.1875 15.9375 5.1875 4.839844 0 9.640625-1.304687 13.949219-3.882813l127.46875-76.183593 127.421875 76.183593c9.324219 5.609376 21.078125 5.097657 29.910156-1.304687 8.855469-6.417969 12.992187-17.449219 10.582031-28.09375l-32.9375-145.089844 111.699219-97.941406c8.214844-7.1875 11.351563-18.539063 7.980469-28.925781zm0 0" fill={theme.colors.warning300}/></svg>
     const StyledDiv = styled('div', () =>{ 
         return {
         height: '100%',
@@ -34,8 +40,12 @@ export default function Projects(props) {
         position: "relative",
         transition: 'ease .2s',
         paddingTop: '15px',
+        ':focus' : {
+            outline: "none !important"
+        },
         ':hover' : {
             transition: 'ease .2s',
+            cursor: 'pointer',
             '::before' : {
                 top: '-2%',
                 left: '-1%',
@@ -68,6 +78,14 @@ export default function Projects(props) {
 
     return (
         <React.Fragment>
+            {selectedProjectIndex != -1 && (
+                <Modal
+                isShowing={isShowing}
+                hide={toggle}
+                project={cardProjects[selectedProjectIndex]}
+                lightTheme={props.lightTheme}
+                ></Modal>
+            )}
             <div style={{zIndex: 5, width: '100%'}}>
             <SectionTitle gridGaps={18}>Projects</SectionTitle>
             <FlexGrid
@@ -79,8 +97,8 @@ export default function Projects(props) {
             marginRight="5vw"
             >
             {cardProjects.map((project, index) => (
-                <FlexGridItem   height='40vh' display="flex">
-                <StyledDiv>
+                <FlexGridItem  height='40vh' display="flex" index={index}>
+                <StyledDiv tabIndex={index} onClick={() => {setSelectedProjectIndex(index); console.log("selected: " + index); toggle()}}>
                     <Grid gridMargins={[6,12,24]} gridGaps={0} gridGutters={0}>
                         <Cell span={[3,7,9]}>
                             <H5 style={{marginTop: 0, marginBottom: 0}}>
@@ -88,24 +106,22 @@ export default function Projects(props) {
                             </H5>
                         </Cell>
                         <Cell span={[1,1,3]} align={ALIGNMENT.start}>
-                            <ParagraphSmall style={{marginTop: 0, marginBottom: 0}}>
-                            <div style={{marginTop: 0, fontSize: '.8em', display: 'flex', justifyContent: 'flex-end'}}>
-                                <em>{project.dateString}</em>
+                            <div style={{height: '100px', width: '100%', marginTop: -5, fontSize: '.8em', display: 'flex', justifyContent: 'flex-end'}}>
+                            <img style={{objectFit: 'contain', width: '100%'}} src={project.thumbnail}></img>
                             </div>
-                            </ParagraphSmall>
                         </Cell>
                     </Grid>
-                    <Grid gridMargins={[6,12,24]} gridGaps={0} gridGutters={0}>
+                    {/* <Grid gridMargins={[6,12,24]} gridGaps={0} gridGutters={0}>
                         <Cell span={[4,8,12]}>
                             <Paragraph1 style={{marginTop: 0, marginBottom: 0}}>
                                 <em>{project.organization ? (project.organization) : (<div>&nbsp;</div>)}</em>
                             </Paragraph1>
                         </Cell>
-                    </Grid>
+                    </Grid> */}
                     <Grid gridMargins={[6,12,24]} gridGaps={0} gridGutters={0}>
                         <Cell span={[4,8,12]}>
-                            <ParagraphSmall style={{marginTop: 0, marginBottom: 0}}>
-                                {project.description.substring(0, 200) + "..."}
+                            <ParagraphSmall style={{marginTop: 10, marginBottom: 0}}>
+                                {project.description.substring(0, 125) + "..."}
                             </ParagraphSmall>
                         </Cell>
                     </Grid>
@@ -118,13 +134,13 @@ export default function Projects(props) {
                             })
                         }
                     }}>
-                    <div style={{height: '100%', display: 'flex', alignItems: 'flex-end'}}>
                         <Cell span={[4,8,12]}>
                             {project.tags.map((tag, index) => (
                                 <Tag closeable={false}
+                                index={index}
                                 overrides={{
                                     Text: {
-                                      style: ({ $theme }) => {
+                                      style: () => {
                                         return {
                                           fontSize: ".8em",
                                         };
@@ -134,7 +150,6 @@ export default function Projects(props) {
                                 >{tag}</Tag>
                             ))}
                         </Cell>
-                        </div>
                     </Grid>
                 </StyledDiv>
                 </FlexGridItem>
