0b83e95597d29adf619a8ee09d37fad1609756ab
mustafa <mustafa.sadriwala@utdallas.edu>
2020-05-25 16:47:44 -0500

    added select for filter

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,6 +11,7 @@ import { Paragraph1, Paragraph2, H1, H2, H5, H6, H4, ParagraphSmall } from 'base
 import { Tag, VARIANT} from 'baseui/tag';
 import {FlexGrid, FlexGridItem} from 'baseui/flex-grid';
 import {Button, SHAPE, KIND} from 'baseui/button';
+import {Select} from 'baseui/select';
 
 
 
@@ -23,21 +24,21 @@ import CARD_PROJECTS from './../assets/projects-metadata.json';
 
 import {ReactComponent as GithubIcon} from './../assets/github.svg';
 import {ReactComponent as LinkIcon} from './../assets/iconfinder_link5_216660.svg'
-import { withStyle } from 'styletron-react';
+
 
 function filterProjects(filter, allCardProjects, cardProjects, setCardProjects) {
     let newCardProjects = cardProjects;
     if(filter === 'all') {
         newCardProjects = allCardProjects;
     }
-    else if( filter === 'featured') {
-        newCardProjects = cardProjects.filter((project) => project.featured);
+    else if(filter === 'featured') {
+        newCardProjects = allCardProjects.filter((project) => project.featured);
     }
     else if(filter === 'academic') {
-        newCardProjects = cardProjects.filter((project) => project.academic);
+        newCardProjects = allCardProjects.filter((project) => project.academic);
     }
     else if(filter ==='non-academic') {
-        newCardProjects = cardProjects.filter((project) => !project.academic);
+        newCardProjects = allCardProjects.filter((project) => !project.academic);
     }
     setCardProjects(newCardProjects);
 }
@@ -48,14 +49,12 @@ export default function Projects(props) {
     const [cardProjects, setCardProjects] = useState(CARD_PROJECTS.filter(project => project.featured));
     const [selectedProjectIndex, setSelectedProjectIndex] = useState(-1);
     const {isShowing, toggle} = useModal();
-    const [showFeatured, setShowFeatured] = useState(true);
+    const [filterValue, setFilterValue] = useState([{label: 'Featured', id: 'featured'}]);
 
     
 
     const accentColor = (props.lightTheme ? theme.colors.accent50 : theme.colors.accent600)
     const hoverAccent = (props.lightTheme ? theme.colors.accent100 : theme.colors.accent500)
-
-    //const Star = <svg height="100%" viewBox="0 -10 511.98685 511" width="100%" xmlns="http://www.w3.org/2000/svg"><path d="m510.652344 185.902344c-3.351563-10.367188-12.546875-17.730469-23.425782-18.710938l-147.773437-13.417968-58.433594-136.769532c-4.308593-10.023437-14.121093-16.511718-25.023437-16.511718s-20.714844 6.488281-25.023438 16.535156l-58.433594 136.746094-147.796874 13.417968c-10.859376 1.003906-20.03125 8.34375-23.402344 18.710938-3.371094 10.367187-.257813 21.738281 7.957031 28.90625l111.699219 97.960937-32.9375 145.089844c-2.410156 10.667969 1.730468 21.695313 10.582031 28.09375 4.757813 3.4375 10.324219 5.1875 15.9375 5.1875 4.839844 0 9.640625-1.304687 13.949219-3.882813l127.46875-76.183593 127.421875 76.183593c9.324219 5.609376 21.078125 5.097657 29.910156-1.304687 8.855469-6.417969 12.992187-17.449219 10.582031-28.09375l-32.9375-145.089844 111.699219-97.941406c8.214844-7.1875 11.351563-18.539063 7.980469-28.925781zm0 0" fill={theme.colors.warning300}/></svg>
 
     const StyledDiv = styled('div', () =>{ 
         return {
@@ -122,7 +121,39 @@ export default function Projects(props) {
                 ></Modal>
             )}
             <div style={{zIndex: 5, width: '100%'}}>
-            <SectionTitle gridGaps={18}>Projects</SectionTitle>
+            <SectionTitle gridGaps={0}>Projects</SectionTitle>
+            <div style={{zIndex: 6, width: '100%',display: 'flex', justifyContent: 'flex-end'}}>
+            <div style={{zIndex: 100, width: '25%', paddingRight: '5vw', paddingBottom: '20px'}}>
+                <Select
+                options={[
+                    {label: "All", id: "all"},
+                    {label: "Featured", id: "featured"},
+                    {label: "Academic", id: "academic"},
+                    {label: "Non-Academic", id: "non-academic"}
+                ]}
+                value={filterValue}
+                onChange={(params) => {setFilterValue(params.value); filterProjects(params.value[0].id, CARD_PROJECTS, cardProjects, setCardProjects); console.log(params.value)}}
+                placeholder="Filter projects"
+                backspaceRemoves={false}
+                clearable={false}
+                deleteRemoves={false}
+                escapeClearsValue={false}
+                overrides={{
+                    Popover: {
+                        props: {
+                            overrides: {
+                                Body: {
+                                    style: {
+                                        zIndex: 8
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }}
+                />
+            </div>
+            </div>
             <FlexGrid
             flexGridColumnCount={[1, 1, 2, 3]}
             flexGridColumnGap="scale800"
@@ -210,13 +241,13 @@ export default function Projects(props) {
             }
             </FlexGrid>
             <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '50px'}}>
-                {showFeatured ? 
+                {filterValue[0].id !== 'all' ? 
                 (<Button kind={KIND.tertiary} shape={SHAPE.pill} 
-                onClick={() => {setShowFeatured(false); filterProjects('all', CARD_PROJECTS, cardProjects, setCardProjects);}}>
+                onClick={() => {setFilterValue([{label: 'All', id: 'all'}]); filterProjects('all', CARD_PROJECTS, cardProjects, setCardProjects);}}>
                     Show All
                 </Button>) : 
                 (<Button kind={KIND.tertiary} shape={SHAPE.pill} 
-                onClick={() => {setShowFeatured(true); filterProjects('featured', CARD_PROJECTS, cardProjects, setCardProjects);}}>
+                onClick={() => {setFilterValue([{label: 'Featured', id: 'featured'}]); filterProjects('featured', CARD_PROJECTS, cardProjects, setCardProjects);}}>
                     Hide
                 </Button>)
                 }
